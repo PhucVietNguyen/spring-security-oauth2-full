678ab071e0bdf54ecd06a1009d7d2ef736018de6
Phuc Nguyen <phuc.nguyen@adamosoft.com>
2022-08-02 16:01:38 +0700

    add resource service oauth2

--- profile-service/src/main/java/com/phucnv/adamo/profileservice/controller/TestController.java
+++ profile-service/src/main/java/com/phucnv/adamo/profileservice/controller/TestController.java
@@ -1,2 +1,26 @@
-package com.phucnv.adamo.profileservice.controller;public class TestController {
+package com.phucnv.adamo.profileservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+  @GetMapping("/message/{text}")
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  public ResponseEntity<?> showMessage(@PathVariable String text) {
+    return ResponseEntity.status(HttpStatus.OK).body(text);
+  }
+
+  @GetMapping(value = "/send/{message}")
+  @PreAuthorize("hasAuthority('READ_PROFILE')")
+  public ResponseEntity<?> send(@PathVariable String message) {
+    return ResponseEntity.status(HttpStatus.OK).body(message);
+  }
 }
